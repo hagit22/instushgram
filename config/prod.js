02f5368663c3e4cb6ecc62dbd27bca3844cb364a
hagit <hagit@ornim.com>
2024-03-21 10:26:41 +0200

    still deploy issues

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 export default {
-    //dbURL: 'mongodb+srv://hagity22:1234@cluster0.qbbrn5d.mongodb.net/test?retryWrites=true&w=majority',
-    dbURL: 'mongodb+srv://hagity22:1234@cluster0.qbbrn5d.mongodb.net/',
+    dbURL: 'mongodb+srv://hagity22:1234@cluster0.qbbrn5d.mongodb.net/test?retryWrites=true&w=majority',
+    //dbURL: 'mongodb+srv://hagity22:1234@cluster0.qbbrn5d.mongodb.net/',
     dbName: 'instushgram_db'
 }
